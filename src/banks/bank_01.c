60bb3efb113d93408901c240095cc0e328d74caa
Andreas Hahn <zatacke@gmail.com>
2022-05-10 00:06:31 +0200

    move info comment to actual image rombank

--- src/banks/bank_01.c
+++ src/banks/bank_01.c
@@ -13,3 +13,10 @@ unsigned char image_02_meta[92];
 unsigned char image_02_meta_echo[92];
 unsigned char image_02_padding[51];
 unsigned char image_02_unused[21];
+
+// https://funtography.online/wiki/Cartridge_RAM
+// position of frame in ram: 0xnF54
+// position of frame in meta: 0xn1B0 -> 84
+// + 92
+// position of frame in ram: 0xnFB0
+// position of frame in meta: 0xn1B0 -> 176
