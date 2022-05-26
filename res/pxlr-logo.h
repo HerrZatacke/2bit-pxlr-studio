6e3e686ce2f2889e9937d4c633529ca1c7fee568
Andreas Hahn <zatacke@gmail.com>
2022-05-26 16:54:42 +0200

    temp remove logo_tiles             logo_map

--- res/pxlr-logo.h
+++ res/pxlr-logo.h
@@ -1,11 +1,12 @@
 #ifndef PXLR_LOGO_H
 #define PXLR_LOGO_H
-
+/*
 #define LOGO_TILE_COUNT 128
 
 #include <gbdk/platform.h>
 
 extern const unsigned char logo_tiles[];
 extern const unsigned char logo_map[];
+*/
 
 #endif
