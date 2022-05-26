b3ddb33cc0d6e5f72b65256f1128b8b5225d4c35
Andreas Hahn <zatacke@gmail.com>
2022-05-26 19:12:01 +0200

    move logo to bank 1

--- src/utils.h
+++ src/utils.h
@@ -129,12 +129,12 @@ void dead(/*unsigned char reason*/) {
 }
 
 inline unsigned char splash() {
-/*
-  set_data(VRAM_9000, logo_tiles, LOGO_TILE_COUNT * 16);
-  set_data(VRAM_8000, logo_tiles, LOGO_TILE_COUNT * 16);
 
-  set_bkg_tiles(0, 0, 20, 18, logo_map);
-*/
+  set_data_banked(VRAM_9000, logo_tiles, LOGO_TILE_COUNT * 16, 1);
+  set_data_banked(VRAM_8000, logo_tiles, LOGO_TILE_COUNT * 16, 1);
+
+  set_bkg_tiles_banked(0, 0, 20, 18, logo_map, 1);
+
   set_bkg_based_tiles(0, 16, sizeof(branch), 1, branch, OFFSET_FONT - 32);
   set_bkg_based_tiles(0, 17, sizeof(version), 1, version, OFFSET_FONT - 32);
   set_bkg_based_tiles(13, 16, 7, 2, "Shoot A Menu B", OFFSET_FONT - 32);
