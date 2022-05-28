a481282c64422aa17419d7dd5488731e56d17364
Andreas Hahn <zatacke@gmail.com>
2022-05-28 20:49:19 +0200

    Move dialog code to bank 1

---
+++ src/dialog.c
@@ -0,0 +1,57 @@
+#pragma bank 1
+
+#include <gb/gb.h>
+#include <gbdk/platform.h>
+#include "./defines.h"
+#include "./joypad.h"
+#include "./utils.h"
+
+void appearDialog() BANKED OLDCALL {
+  move_win(7, 146);
+
+  for (unsigned char i = 0; i < 8; i += 1) {
+
+    if (i == 4) {
+      beep();
+    }
+
+    scroll_win(0, -7);
+    wait_vbl_done();
+  }
+
+  waitRelease();
+}
+
+void disappearDialog() BANKED OLDCALL {
+  waitRelease();
+
+  for (unsigned char i = 0; i < 8; i += 1) {
+    scroll_win(0, 7);
+    wait_vbl_done();
+  }
+
+  move_win(7, 146);
+}
+
+unsigned char dialog(unsigned char *message) BANKED OLDCALL {
+  fill_win_rect(0, 0, 20, 1, MENU_BORDER_TOP);
+  fill_win_rect(0, 1, 20, 10, OFFSET_BLANK);
+  set_win_based_tiles(1, 2, 16, 1, message, OFFSET_FONT - 32);
+  set_win_based_tiles(14, 4, 5, 2, "Yes A No B", OFFSET_FONT - 32);
+
+  appearDialog();
+
+  while (1) {
+    wait_vbl_done();
+
+    if (jp == J_B) {
+      boop();
+      disappearDialog();
+      return 0;
+    } else if (jp == J_A) {
+      beep();
+      disappearDialog();
+      return 1;
+    }
+  }
+}
--- src/dialog.h
+++ src/dialog.h
@@ -1,49 +1,10 @@
-inline void appearDialog() {
-  move_win(7, 146);
+#ifndef DIALOG_H
+#define DIALOG_H
 
-  for (unsigned char i = 0; i < 8; i += 1) {
+#include <gbdk/platform.h>
 
-    if (i == 4) {
-      beep();
-    }
+void appearDialog() BANKED OLDCALL;
+void disappearDialog() BANKED OLDCALL;
+unsigned char dialog(unsigned char *message) BANKED OLDCALL;
 
-    scroll_win(0, -7);
-    wait_vbl_done();
-  }
-
-  waitRelease();
-}
-
-inline void disappearDialog() {
-  waitRelease();
-
-  for (unsigned char i = 0; i < 8; i += 1) {
-    scroll_win(0, 7);
-    wait_vbl_done();
-  }
-
-  move_win(7, 146);
-}
-
-unsigned char dialog(unsigned char *message) {
-  fill_win_rect(0, 0, 20, 1, MENU_BORDER_TOP);
-  fill_win_rect(0, 1, 20, 10, OFFSET_BLANK);
-  set_win_based_tiles(1, 2, 16, 1, message, OFFSET_FONT - 32);
-  set_win_based_tiles(14, 4, 5, 2, "Yes A No B", OFFSET_FONT - 32);
-
-  appearDialog();
-
-  while (1) {
-    wait_vbl_done();
-
-    if (jp == J_B) {
-      boop();
-      disappearDialog();
-      return 0;
-    } else if (jp == J_A) {
-      beep();
-      disappearDialog();
-      return 1;
-    }
-  }
-}
+#endif
