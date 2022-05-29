870fec7cddc345fb0d9da53842aefd6304da1266
Andreas Hahn <zatacke@gmail.com>
2022-05-29 15:44:57 +0200

    whitespaces

--- src/menus/shootingManualMenuItems.c
+++ src/menus/shootingManualMenuItems.c
@@ -17,13 +17,13 @@ MenuItem ditherSetsMenu =     {  7, 1, 0,  4,      NUM_DITHERSETS,  1, "Dither",
 MenuItem voltageRefsMenu =    { 14, 1, 0,  6,    NUM_VOLTAGE_REFS,  3, " V-Ref", "  VoltageReference",    &voltageRefs[0], MENU_ACTION_NONE, };
 
 MenuItem invertOutputsMenu =  {  4, 2, 0, 11,  NUM_INVERT_OUTPUTS,  0, "InvOut", "   Invert   Output",  &invertOutputs[0], MENU_ACTION_NONE, };
-MenuItem zeroPointsMenu =     {  11, 2, 0,  7,     NUM_ZERO_POINTS,  1, "0Point", "     Zero   Points",     &zeroPoints[0], MENU_ACTION_NONE, };
+MenuItem zeroPointsMenu =     {  11, 2, 0, 7,     NUM_ZERO_POINTS,  1, "0Point", "     Zero   Points",     &zeroPoints[0], MENU_ACTION_NONE, };
 
 MenuItem edgeOpModesMenu =    {  0, 3, 0,  9,   NUM_EDGE_OP_MODES,  3, "EdOper", "     EdgeOperation",    &edgeOpModes[0], MENU_ACTION_NONE, };
 MenuItem edgeModesMenu =      {  7, 3, 0,  5,      NUM_EDGE_MODES,  0, "EdMode", "     Edge     Mode",      &edgeModes[0], MENU_ACTION_NONE, };
 MenuItem edgeExclusivesMenu = { 14, 3, 0, 10,  NUM_EDGE_EXCLUSIVE,  1, "EdExcl", "     EdgeExclusive", &edgeExclusives[0], MENU_ACTION_NONE, };
 
-MenuItem captureModesMenu =   { 0, 0, 0, 12,   NUM_CAPTURE_MODES,  0, "Captur", "  Capture     Mode",   &captureModes[0], MENU_ACTION_NONE, };
+MenuItem captureModesMenu =   {  0, 0, 0, 12,   NUM_CAPTURE_MODES,  0, "Captur", "  Capture     Mode",   &captureModes[0], MENU_ACTION_NONE, };
 
 MenuItem *menuItems[NUM_MENU_ELEMENTS] = {
   &exposureTimesMenu,
