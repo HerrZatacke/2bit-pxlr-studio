8af1566dafc7abc5bc77baeff7561587fe8a01bd
Andreas Hahn <zatacke@gmail.com>
2022-06-15 12:57:24 +0200

    contrast 9 as default

--- src/menus/shootingAssistedMenuItems.c
+++ src/menus/shootingAssistedMenuItems.c
@@ -6,7 +6,7 @@
 #define NUM_MENU_ELEMENTS_ASSISTED 3
 
 MenuItem assistedOptionsMenu =   {  0, 0, 0, 2, NUM_ASSISTED_OPTIONS, 22, "   Set", " Exposure  Setting", &assistedOptions[0], 0, };
-MenuItem contrastsMenuAssisted = {  7, 1, 0, 3,        NUM_CONTRASTS,  7, "Cntrst", "   Dither Contrast",       &contrasts[0], 0, };
+MenuItem contrastsMenuAssisted = {  7, 1, 0, 3,        NUM_CONTRASTS,  8, "Cntrst", "   Dither Contrast",       &contrasts[0], 0, };
 MenuItem ditherToggleMenu =      { 14, 1, 0, 4,    NUM_DITHER_TOGGLE,  0, "Dither", "   Dither   Toggle",    &ditherToggle[0], 0, };
 
 MenuItem *menuItemsAssisted[NUM_MENU_ELEMENTS_ASSISTED] = {
