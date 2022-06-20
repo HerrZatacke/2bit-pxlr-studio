40012eb39c5c4da6e33981a1c909b509d3695e6a
Toxa <untoxa@mail.ru>
2022-06-20 13:48:31 +0300

    add flash_ prefix to flasher functions;  flash_erase_slot() function erases the gallery in the slot.

--- src/flasher.c
+++ src/flasher.c
@@ -22,7 +22,7 @@ inline uint8_t slot_to_bank(uint8_t slot, uint8_t ofs) {
     return (((slot + 2) << 1) + ofs) << 2;
 }
 
-uint8_t check_gallery_exist(uint8_t slot) NONBANKED {
+uint8_t flash_check_gallery_exist(uint8_t slot) NONBANKED {
     uint8_t _save = _current_bank, result;
     SWITCH_ROM(slot_to_bank(slot, 0));
     result = ((saved_game_data.magic[0] == 'M') &&
@@ -41,13 +41,17 @@ void restore_sram_bank(uint8_t bank) NONBANKED {
     SWITCH_ROM(_save);
 }
 
-uint8_t load_gallery_from_slot(uint8_t slot) BANKED {
+uint8_t flash_load_gallery_from_slot(uint8_t slot) BANKED {
+    // check saved gallery exist
+    if (!flash_check_gallery_exist(slot)) return FALSE;
+    // read 8 SRAM banks from the flash sector
     save_rom_bank = slot_to_bank(slot, 0);
     save_sram_bank_offset = FIRST_HALF_OFS;
     for (uint8_t i = 0; i < FIRST_HALF_LEN; i++) {
         SWITCH_RAM(i + FIRST_HALF_OFS);
         restore_sram_bank(i);
     }
+    // read the next 8 SRAM banks from the next flash sector
     save_rom_bank = slot_to_bank(slot, 1);
     save_sram_bank_offset = SECOND_HALF_OFS;
     for (uint8_t i = 0; i < SECOND_HALF_LEN; i++) {
@@ -60,7 +64,7 @@ uint8_t load_gallery_from_slot(uint8_t slot) BANKED {
 extern uint8_t erase_flash() OLDCALL;                   // erases FLASH sector: 64K or 4 banks
 extern uint8_t save_sram_banks(uint8_t count) OLDCALL;  // copies up to count SRAM banks to FLASH
 
-uint8_t save_gallery_to_slot(uint8_t slot) BANKED {
+uint8_t flash_save_gallery_to_slot(uint8_t slot) BANKED {
     // erase the sector and save first 8 SRAM banks
     save_sram_bank_offset = FIRST_HALF_OFS;
     save_rom_bank = slot_to_bank(slot, 0);
@@ -71,4 +75,11 @@ uint8_t save_gallery_to_slot(uint8_t slot) BANKED {
     save_rom_bank = slot_to_bank(slot, 1);
     if (!erase_flash()) return FALSE;
     return save_sram_banks(SECOND_HALF_LEN);            // update offset
+}
+
+uint8_t flash_erase_slot(uint8_t slot) BANKED {
+    save_rom_bank = slot_to_bank(slot, 0);
+    if (!erase_flash()) return FALSE;
+    save_rom_bank = slot_to_bank(slot, 1);
+    return erase_flash();
 }
