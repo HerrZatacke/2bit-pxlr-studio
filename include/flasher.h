048ced0d13852f8ccf9d6d1090e76bf1d2d953a7
Toxa <untoxa@mail.ru>
2022-06-20 13:48:31 +0300

    add flash_ prefix to flasher functions;  flash_erase_slot() function erases the gallery in the slot.

--- include/flasher.h
+++ include/flasher.h
@@ -3,8 +3,9 @@
 
 #include <stdint.h>
 
-uint8_t check_gallery_exist(uint8_t slot) NONBANKED;
-uint8_t load_gallery_from_slot(uint8_t slot) BANKED;
-uint8_t save_gallery_to_slot(uint8_t slot) BANKED;
+uint8_t flash_check_gallery_exist(uint8_t slot) NONBANKED;
+uint8_t flash_load_gallery_from_slot(uint8_t slot) BANKED;
+uint8_t flash_save_gallery_to_slot(uint8_t slot) BANKED;
+uint8_t flash_erase_slot(uint8_t slot) BANKED;
 
 #endif
